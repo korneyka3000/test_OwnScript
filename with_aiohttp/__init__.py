bbbd4aad8fd23f9a4397e71c856bc0faa74cd93a
korneyka3000 <korneyburau@gmail.com>
2023-03-22 16:30:56 +0300

    deleted logging in file

--- with_aiohttp/__init__.py
+++ with_aiohttp/__init__.py
@@ -5,6 +5,4 @@ from .lighter import Lighter
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
-    filename="api_lighter.log",
-    filemode="w",
 )
